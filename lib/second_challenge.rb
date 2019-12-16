7fd2b1f744f21a29fdfb0093ce3669500f30da2f
Simon Tucker <sjtucker28@gmail.com>
2019-12-16 15:02:12 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,13 +4,13 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_frineds => []
+      :hero_friends => []
     },
     :capulet => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_frineds => []
+      :hero_friends => []
     }
   }
 end
