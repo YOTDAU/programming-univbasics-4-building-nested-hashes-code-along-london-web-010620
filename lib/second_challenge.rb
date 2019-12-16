bba94c1bf6727f97d8ae5280b5f50ca049b985b5
Simon Tucker <sjtucker28@gmail.com>
2019-12-16 15:01:25 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_frineds => []
+    }
+    :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_frineds => []
     }
   }
+end
