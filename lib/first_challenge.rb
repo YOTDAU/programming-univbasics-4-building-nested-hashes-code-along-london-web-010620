adfce28ed208dc8e2fd4b8a2c4d4b77a31b0b7a9
Simon Tucker <sjtucker28@gmail.com>
2019-12-16 14:54:27 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,12 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
+       :patriarch => {},
+       :matriarch => {},
+       :hero => {},
+       :hero_friends => {}
+     }
+     
      
      :capulet => {}
    }
