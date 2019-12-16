230f76c151f0479bf8a16a48364a896829f3430e
Simon Tucker <sjtucker28@gmail.com>
2019-12-16 14:49:35 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,11 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {},
+     :montague => {
+       :partriarch => {
+         :name => "Lord Montague"
+         :age => "53"
+       }
+     },
      :capulet => {}
    }
 end
